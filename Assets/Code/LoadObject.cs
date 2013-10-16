31e09c3b60232e80bfc60608881183dab39a1fa7
kasiu <kasiu@gatech.edu>
2013-10-16 19:05:28 -0400

    Adding dynamic layout/title screen. Reloc. commit.

--- Assets/Code/LoadObject.cs
+++ Assets/Code/LoadObject.cs
@@ -19,20 +19,38 @@ public class LoadObject : MonoBehaviour {
     /// </summary>
     public List<string> tagNames = new List<string>();
 
+    public List<Vector3> layoutPositions = new List<Vector3>();
+
 	// Use this for initialization
 	void Start () {
-        foreach (string objName in objectNames) {
-            // TODO: smarter layout choices OR fixed grid
-            GameObject newObject = (GameObject)GameObject.Instantiate(prefab, 
-                transform.position, transform.rotation);
-            newObject.AddComponent("ScoreTriggerTagAgreement");
+        //int layoutCounter = 0;
+        //foreach (string objName in objectNames) {
+        //    // TODO: smarter layout choices OR fixed grid
+        //    Vector3 position = GenerateRandomPosition(layoutPositions[layoutCounter]);
+        //    GameObject newObject = (GameObject)GameObject.Instantiate(prefab, 
+        //        position, Quaternion.identity);
+        //    newObject.AddComponent("ScoreTriggerTagAgreement");
 
-            newObject.name = objName;
-            newObject.transform.FindChild("Tag1").name = tagNames[0];
-            newObject.transform.FindChild("Tag2").name = tagNames[1];
-            if (newObject.transform.FindChild("Tag3") != null) {
-                newObject.transform.FindChild("Tag3").name = tagNames[2];
-            }
-        }
+        //    newObject.name = objName;
+        //    newObject.transform.FindChild("Tag1").name = tagNames[0];
+        //    //newObject.transform.FindChild("Tag1Label").guiText.text = tagNames[0];
+        //    newObject.transform.FindChild("Tag2").name = tagNames[1];
+        //    if (newObject.transform.FindChild("Tag3") != null) {
+        //        newObject.transform.FindChild("Tag3").name = tagNames[2];
+        //    }
+        //    layoutCounter++;
+        //}
 	}
+
+    private const float maxRandX = 0.01f;
+    private const float maxRandY = 0.02f;
+
+    private Vector3 GenerateRandomPosition(Vector3 position) {
+        Vector3 newPosition = position;
+        float randX = Random.Range(-maxRandX, maxRandX);
+        float randY = Random.Range(-maxRandY, maxRandY);
+        position.x += randX;
+        position.y += randY;
+        return position;
+    }
 }
