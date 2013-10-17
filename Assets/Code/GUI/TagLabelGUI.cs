3dd55c5b111fe7558220108ed2d2304cae1cf143
kasiu <kasiu@gatech.edu>
2013-10-17 18:10:13 -0400

    Added tag labeling. Layout broken. Reloc. commit.

---
+++ Assets/Code/GUI/TagLabelGUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TagLabelGUI : MonoBehaviour {
+    /// <summary>
+    /// Text size.
+    /// </summary>
+    public int fontSize = 10;
+
+    public float x, y;
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    /// <summary>
+    /// Draws score on screen
+    /// </summary>
+    void OnGUI() {
+        GUIStyle style = new GUIStyle();
+        style.fontSize = fontSize;
+
+        Vector3 pixelPosition = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
+        // NOTE (kasiu): Screen.height - pixelPosition.y sets the y position correctly because FOO SCREENSPACE axes reversal :(
+        GUI.Box(new Rect(pixelPosition.x, Screen.height - pixelPosition.y, 100, 100), this.gameObject.name, style);
+    }
+}
