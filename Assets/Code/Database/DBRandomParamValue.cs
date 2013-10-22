7d4e761b85c4e90a9a20baa256abe5bf9576ee11
alex zook <zook.ae@gmail.com>
2013-10-22 15:37:55 -0400

    added level restarts and random paramter initialization

---
+++ Assets/Code/Database/DBRandomParamValue.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum ParamType {
+    BULLETSPEED,
+    BULLETSIZE,
+    FIRERATE
+}
+
+public class DBRandomParamValue : ParameterSlider {
+
+    public List<Shoot> paramArray = new List<Shoot>();
+
+    public string entity = "Enemy";
+
+    private float randomValue;
+    public ParamType ptype;
+
+    void Awake() {
+        randomValue = Random.Range(0.0f, 1.0f); // generate random value
+        float newValue = (paramMax - paramMin) * randomValue; // set to value in scaling range
+
+        GameObject[] objs = GameObject.FindGameObjectsWithTag(entity);
+        foreach (GameObject o in objs) {
+            paramArray.Add(o.GetComponent<Shoot>());
+        }
+
+        Debug.Log("setting " + ptype.ToString() + " to value: " + newValue);
+
+        foreach (Shoot p in paramArray) {
+            if (p == null)
+                continue;
+            switch (ptype) {
+                case ParamType.BULLETSPEED:
+                    p.bulletSpeed = newValue;
+                    break;
+                case ParamType.BULLETSIZE:
+                    p.spawn.transform.localScale = new Vector3(newValue, newValue);
+                    break;
+                case ParamType.FIRERATE:
+                    p.gameObject.GetComponent<NPCShootInDirection>().frequency = newValue;
+                    break;
+            }
+            
+        }
+    }
+
+}
---
+++ Assets/Code/Score/RestartEntityCount.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RestartEntityCount : MonoBehaviour {
+
+    /// <summary>
+    /// Tag of entity that must be present at count amount
+    /// </summary>
+    public string countTag;
+
+    /// <summary>
+    /// Amount of quantity to trigger restart
+    /// </summary>
+    public int countAmount;
+
+    private GameObject[] targetSet;
+
+    private float checkFreq = 1;
+    private float timeDelta = 0.0f;
+
+	void Awake () {
+        targetSet = GameObject.FindGameObjectsWithTag(countTag);
+	}
+	
+	void Update () {
+        timeDelta += Time.deltaTime;
+
+        // check periodically
+        if (timeDelta > checkFreq) {
+            timeDelta = 0.0f;
+
+            targetSet = GameObject.FindGameObjectsWithTag(countTag); // update set of objects
+            if (targetSet.Length == countAmount) {
+                Application.LoadLevel(Application.loadedLevel); // restart level
+            }
+        }
+	}
+}
