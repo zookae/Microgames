cdee37005298da9a3c6e17b4d005284064a0dcb4
alex zook <zook.ae@gmail.com>
2013-10-30 14:16:35 -0400

    disabling annoying editor action

--- Assets/Editor/ShowObjectSelection.cs
+++ Assets/Editor/ShowObjectSelection.cs
@@ -7,25 +7,25 @@ class ShowObjectSelection : MonoBehaviour {
     // Rotates the selected Game Object +45 degrees if the user presses 'g'
 	// or -45 degrees if the user presses 'Shift + g'
 	// If no object is selected, the Menus are grayed out.
-	[MenuItem("Example/Rotate Green +45 _g")]
-	static void RotateGreenPlus45() {
-		var obj = Selection.activeGameObject;
-		obj.transform.Rotate(Vector3.up*45);
-	}
+    //[MenuItem("Example/Rotate Green +45 _g")]
+    //static void RotateGreenPlus45() {
+    //    var obj = Selection.activeGameObject;
+    //    obj.transform.Rotate(Vector3.up*45);
+    //}
 
-	[@MenuItem ("Example/Rotate Green +45 _g", true)]
-	static bool ValidatePlus45() {
-		return Selection.activeGameObject != null;
-	}
+    //[@MenuItem ("Example/Rotate Green +45 _g", true)]
+    //static bool ValidatePlus45() {
+    //    return Selection.activeGameObject != null;
+    //}
 
-	[@MenuItem ("Example/Rotate green -45 #g")]
-    static void RotateGreenMinus45() {
-		var obj = Selection.activeGameObject;
-		obj.transform.Rotate(Vector3.down*45);
-	}
-	[@MenuItem ("Example/Rotate green -45 #g", true)]
-	static bool ValidateMinus45() {
-		return Selection.activeGameObject != null;
-	}
+    //[@MenuItem ("Example/Rotate green -45 #g")]
+    //static void RotateGreenMinus45() {
+    //    var obj = Selection.activeGameObject;
+    //    obj.transform.Rotate(Vector3.down*45);
+    //}
+    //[@MenuItem ("Example/Rotate green -45 #g", true)]
+    //static bool ValidateMinus45() {
+    //    return Selection.activeGameObject != null;
+    //}
 
 }
