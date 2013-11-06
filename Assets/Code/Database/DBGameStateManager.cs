881358dda2be5a2b8920b8a546816833c29593ab
kasiu <kasiu@gatech.edu>
2013-11-06 01:58:53 -0500

    Made some changes to SNG.

---
+++ Assets/Code/Database/DBGameStateManager.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DBGameStateManager : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+
+    // TODO (kasiu): Use this to replace LoadProxyGame's setup logic
+    void Awake() {
+        // TODO (kasiu): Need to write a string parser.
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (GameState.Singleton.CurrentState == State.Win ||
+            GameState.Singleton.CurrentState == State.Lose) {
+            // Send trace to the database if it hasn't already.
+        }
+	}
+}
