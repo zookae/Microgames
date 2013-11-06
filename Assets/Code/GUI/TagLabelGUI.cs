ed87d00b895924b1690af8e1146f14423b015051
kasiu <kasiu@gatech.edu>
2013-11-05 21:11:52 -0500

    Fixed the stupid text centering bug. FINALLY.

--- Assets/Code/GUI/TagLabelGUI.cs
+++ Assets/Code/GUI/TagLabelGUI.cs
@@ -7,7 +7,10 @@ public class TagLabelGUI : MonoBehaviour {
     /// </summary>
     public int fontSize = 10;
 
-    public float x, y;
+    /// <summary>
+    /// The default font for the label
+    /// </summary>
+    public Font defaultFont = Resources.Load("../Fonts/Nunito-Regular") as Font;
 
     /// <summary>
     /// If the tag name contains spaces, we store the individual words.
@@ -23,14 +26,25 @@ public class TagLabelGUI : MonoBehaviour {
     /// Draws score on screen
     /// </summary>
     void OnGUI() {
+        // Sets up style
         GUIStyle style = new GUIStyle();
         style.fontSize = fontSize;
+        style.alignment = TextAnchor.MiddleCenter;
+        if (defaultFont != null) {
+            style.font = defaultFont;
+        }
 
         Vector3 pixelPosition = Camera.main.WorldToScreenPoint(this.gameObject.transform.position);
         // NOTE (kasiu): Screen.height - pixelPosition.y sets the y position correctly because FOO SCREENSPACE axes reversal :(
-        // TODO (kasiu): YES, IT'S LEFT-JUSTIFIED FONT. DEAL WITH IT.
         for (var i = 0; i < splitName.Length; i++) {
-            GUI.Box(new Rect(pixelPosition.x, Screen.height + (fontSize * i) - (pixelPosition.y), 100, 100), splitName[i], style);
+            Vector2 p = new Vector2(pixelPosition.x, Screen.height + (fontSize * i) - (pixelPosition.y));
+
+            // The following centers the font.
+            // HACK (kasiu): The font "height" is not always correct given the font. Check when using different fonts.
+            Vector2 tweak = new Vector2(splitName[i].Length * fontSize, fontSize * 2);
+            tweak /= 2.0f;
+            p -= tweak;
+            GUI.Box(new Rect(p.x, p.y, fontSize * splitName[i].Length, fontSize * 2), splitName[i], style); //, style);
         }
     }
 }
