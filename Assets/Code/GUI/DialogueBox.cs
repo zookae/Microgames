22b2ca680898adb84653d37f57a7c0dc94e25dcb
kasiu <kasiu@gatech.edu>
2013-11-07 06:24:11 -0500

    Some early tutorial assets.

---
+++ Assets/Code/GUI/DialogueBox.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DialogueBox : MonoBehaviour {
+
+    public Vector2 position;
+    public string text;
+    public bool active;
+    private bool closed;
+
+	// Use this for initialization
+	void Start () {
+        closed = false;
+        active = false;
+	}
+
+    void ToggleRenderable() {
+    }
+
+    void OnGUI() {
+        if (active && !closed) {
+            // Draw
+        }
+    }
+
+    void OnMouseUpAsButton() {
+        closed = true;
+        active = false;
+    }
+   
+	
+	// Update is called once per frame
+	void Update () {}
+}
---
+++ Assets/Code/Scene/TutorialManager.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TutorialManager : MonoBehaviour {
+
+    public List<DialogueBox> dialogueBoxes;
+    public int currentBoxIndex;
+
+	// Use this for initialization
+	void Start () {
+        GameState.Singleton.CurrentState = State.Paused;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+}
